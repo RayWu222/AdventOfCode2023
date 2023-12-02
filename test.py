55e16b782c52ee8ca661cbb2df4f46439cc77d63
RayWu222 <48284351+RayWu222@users.noreply.github.com>
2023-12-02 02:13:27 -0800

    Part 1 Python Solution

---
+++ test.py
@@ -0,0 +1,26 @@
+file = open("input.txt", "r")
+lines = file.readlines()
+count = 0
+total = 0 
+for line in lines:
+    # char_line = line.encode('utf-8')
+    charArray = [*line]
+    length =  len(charArray)
+    left = 0
+    right = 0
+    for i in range(0, length):
+        if ord(charArray[i]) >= 48 and ord(charArray[i]) <= 57:
+            left = int(line[i]) * 10
+            
+            break
+    for i in range(length-1, -1, -1):
+       
+        if ord(charArray[i]) >= 48 and ord(charArray[i]) <= 57:
+            right = int(line[i])
+            break
+    
+    total += left + right
+    # print('left: ' + str(left) + " | right: " + str(right) + " | total = " + str(total))
+print(total)
+        
+
